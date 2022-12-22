8ceb91762a4bc31bdea3c749ce8b33e2015d62a5
Tatyana58 <tatbir@rambler.ru>
2022-12-22 23:32:01 +0300

    Начало Все четные числа увеличиваем на единицу Возвращаем кусок списка с 3-го по 7-й элемент Возвращаем кусок списка с 3-го по 7-й элемент в методе refundArray

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Заданный массив :");
         int[] arr = {2,4,7,8,12,45,36,47,86,30,34,31,46,65,37,18};
         for (int y = 0; y < arr.length; y++) {
         System.out.print(arr[y]+",");
         }
         System.out.println("\b\n");
         add1(arr);
+        System.out.println("\n");
         refundArray(arr);
-
-        //System.out.println(add1(arr));
     }
     public static int add1(int [] arr) {
         int x = 0;
@@ -18,24 +17,18 @@ public class Main {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 2 == 0) {
                 arr[i]++;
-                if (i > 1 && i < 7) {
-                    x = arr[i];
-                }
+                x = arr[i];
             } else {
-                if (i > 1 && i < 7) {
-                    x = arr[i];
-                }
+                x = arr[i];
             }
-           //Возвращаем кусок списка с 3-го по 7-й элемент.
-            if (i > 1 && i < 7) {
                 System.out.print(x + ",");
-           }
         }
         return x;
     }
     //Возвращаем кусок списка с 3-го по 7-й элемент.
     public static int refundArray(int [] arr) {
         int x= 0;
+        System.out.println("Возвращаем кусок списка с 3-го по 7-й элемент.");
         for (int i = 0; i < arr.length; i++) {
             if (i > 1 && i < 7) {
                 x = arr[i];
